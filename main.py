2bd6d1dc22261379e35e47116b00f6657d1f2a0b
yiminliu2004 <yl10843@nyu.edu>
2026-02-22 11:13:46 -0500

    change port

--- main.py
+++ main.py
@@ -78,5 +78,5 @@ def on_startup():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
 
